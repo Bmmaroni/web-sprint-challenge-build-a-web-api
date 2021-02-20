61579ff92437ac224fa1e2e0ef4ce70c24fd8599
Brandon Maroni <63432771+Bmmaroni@users.noreply.github.com>
2021-02-19 19:14:13 -0700

    finished projects endpoints

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,32 +1,50 @@
 // Write your "projects" router here!
 const express = require("express")
 const projects = require("./projects-model")
-const actions = require("../actions/actions-model")
+const { validateProject, validateProjectId } = require("../middleware/middleware")
 
 const router = express.Router()
 
 router.get("/api/projects", (req, res) => {
-
+  projects.get()
+        .then(projects => {
+          res.json(projects)
+        })
+        .catch( err => {next(err)})
 })
 
-router.get("/api/projects/:id", (req, res) => {
-
+router.get("/api/projects/:id", validateProjectId(), (req, res) => {
+  res.json(res.project)
 })
 
-router.post("/api/projects", (req, res) => {
-
+router.post("/api/projects", validateProject(), (req, res) => {
+  projects.insert(req.body) // is a req.params.id needed here?
+        .then(project => {
+          res.status(201).json(project)
+        })
+        .catch( err => {next(err)})
 })
 
-router.put("/api/projects/:id", (req, res) => {
-
+router.put("/api/projects/:id", validateProject(), validateProjectId(), (req, res) => {
+  projects.update(req.params.id, req.body)
+        .then(project => {
+          res.status(200).json(project)
+        })
+        .catch( err => {next(err)})
 })
 
 router.delete("/api/projects/:id", (req, res) => {
-
+  projects.delete(req.params.id)
+        .then(project => {res.status(204).end()})
+        .catch( err => {next(err)})
 })
 
-router.get("/api/projects/:id/actions", (req, res) => {
-  
+router.get("/api/projects/:id/actions", validateProjectId(), (req, res) => {
+  projects.getProjectActions(req.params.id)
+        .then(actions => {
+          res.status(200).json(actions)
+        })
+        .catch(err => {next(err)})
 })
 
 module.exports = router
